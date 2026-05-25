75953b41729b46afa28c4533967d87976d07ef96
Joao Victor Sena <jvsena16@gmail.com>
2026-05-25 07:58:46 -0300

    chore: bump version to 0.11.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ val localProperties = Properties().apply {
     if (file.exists()) load(file.inputStream())
 }
 
-val appVersionName = "0.11.0"
+val appVersionName = "0.11.1"
 
 plugins {
     alias(libs.plugins.android.application)
@@ -22,7 +22,7 @@ android {
         applicationId = "com.github.jvsena42.mandacaru"
         minSdk = 29
         targetSdk = 36
-        versionCode = 22
+        versionCode = 23
         versionName = appVersionName
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
