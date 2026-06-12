50f6bcbb2a2c321177db56dbbf9d0dffc76f3235
Alwin <thebutterzone@cox.net>
2026-06-12 11:41:40 +0000

    Update UpdateDownloadState.kt

--- app/src/main/java/com/github/jvsena42/mandacaru/data/update/UpdateDownloadState.kt
+++ app/src/main/java/com/github/jvsena42/mandacaru/data/update/UpdateDownloadState.kt
@@ -2,5 +2,7 @@ package com.github.jvsena42.mandacaru.data.update
 
 data class UpdateDownloadState(
     val version: String,
-    val downloadId: Long
+    val downloadId: Long,
+    val isCompleted: Boolean = false,
+    val uri: String? = null
 )
