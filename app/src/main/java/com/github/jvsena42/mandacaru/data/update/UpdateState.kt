904b6c6ed7fab35e3aa3ad9e468a304f107ae51d
Alwin <thebutterzone@cox.net>
2026-06-12 12:04:06 +0000

    Delete app/src/main/java/com/github/jvsena42/mandacaru/data/update/UpdateState.kt

--- app/src/main/java/com/github/jvsena42/mandacaru/data/update/UpdateState.kt
+++
@@ -1,6 +0,0 @@
-sealed class UpdateState {
-    data object NoUpdate : UpdateState()
-    data object Downloading : UpdateState()
-    data class ReadyToInstall(val uri: Uri) : UpdateState()
-    data object Available : UpdateState()
-}
